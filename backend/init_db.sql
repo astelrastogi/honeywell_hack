d7f3d91f4e3e7364449ed09077ec877f0eda5179
Aastha Rastogi <aasthar3@gmail.com>
2025-04-06 01:10:45 -0700

    add actual iot-sensor params

--- backend/init_db.sql
+++ backend/init_db.sql
@@ -4,9 +4,6 @@ CREATE TABLE machine_status (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     timestamp TEXT,
     temperature REAL,
-<<<<<<< Updated upstream
-    pressure REAL
-=======
     pressure REAL,
     soundLevel REAL,
     waterLevel REAL,
@@ -16,26 +13,12 @@ CREATE TABLE machine_status (
     uptime TEXT,
     efficiency REAL,
     operator TEXT
->>>>>>> Stashed changes
 );
--- vibration REAL,
-    -- power REAL,
-    -- status TEXT,
-    -- uptime TEXT,
-    -- efficiency TEXT,
-    -- operator TEXT
+
 
 -- Dummy data
-<<<<<<< Updated upstream
-INSERT INTO machine_status (timestamp, temperature, pressure)
--- vibration, power, status, uptime, efficiency, operator
 
-VALUES (
-    datetime('now'), 72.4, 143.2
-=======
 INSERT INTO machine_status (timestamp, temperature, pressure, soundLevel, waterLevel, distance, potentiometer, status, uptime, efficiency, operator)
 VALUES (
     datetime('now'), 72.4, 143.2, 55, 1, 20, 0.5, 'Operational', '14d 6h 23m', '87%', 'OP-4872'
->>>>>>> Stashed changes
 );
- -- 0.32, 57.3, 'Operational', '14d 6h 23m', '87%', 'OP-4872'
--- backend/models.py
+++ backend/models.py
@@ -6,11 +6,7 @@ def get_latest_status():
     cursor = conn.cursor()
 
     cursor.execute("""
-<<<<<<< Updated upstream
-        SELECT timestamp, temperature, pressure
-=======
         SELECT timestamp, temperature, pressure, soundLevel, waterLevel, distance, potentiometer, status, uptime, efficiency, operator
->>>>>>> Stashed changes
         FROM machine_status
         ORDER BY timestamp DESC
         LIMIT 1
@@ -22,19 +18,4 @@ def get_latest_status():
     if not row:
         return None
     return dict(row)
-<<<<<<< Updated upstream
-    # return {
-    #     "timestamp": row["timestamp"],
-    #     "temperature": f"{row['temperature']} °C",
-    #     "pressure": f"{row['pressure']} PSI",
-    #     "vibration": f"{row['vibration']} mm/s",
-    #     "power": f"{row['power']} kW",
-    #     "status": row["status"],
-    #     "uptime": row["uptime"],
-    #     "efficiency": row["efficiency"],
-    #     "operator": row["operator"],
-    #     "message": "Data loaded from database!"
-    # }
-=======
->>>>>>> Stashed changes
 
